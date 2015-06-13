4efcbe89fe8c267d40c9c29111f3c8276d4b7eee
adadesions <ada_dev@icloud.com>
2015-06-13 21:56:50 +0700

    update about page, navbar, brochure, shotcuts

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,17 @@ router.get('/home', function(req, res, next) {
   res.render('index');
 });
 
+router.get('/brochure', function(req, res, next) {
+  res.render('brochure');
+});
+
+router.get('/about/:sec', function(req, res, next) {
+	if(req.params.sec == 'knows')
+		res.render('about/knows');
+	else if(req.params.sec == 'why')
+		res.render('about/why');
+	else
+		res.render('index');
+});
+
 module.exports = router;
